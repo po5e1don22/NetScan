5106249912b98887ce08f5da482699feffb68e9c
po5e1don22 <po5e1don22@yandex.ru>
2026-04-18 23:03:47 +0300

    add: unique directory name generator

--- src/suricata/suricata_runner.cpp
+++ src/suricata/suricata_runner.cpp
@@ -1,9 +1,28 @@
 #include <cstdlib>
 #include <iostream>
 #include <filesystem>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
 
-bool run_suricata(const std::string& pcap_file, const std::string& output_dir)
+//unique directory name generator 
+std::string generate_output_dir (const std::string& base_dir)
 {
+    auto now = std::chrono::system_clock::now();
+    std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+    std::tm local_time{};
+    localtime_r(&time, &local_time);
+
+    std::stringstream ss;
+    ss <<base_dir << "/run_" << std::put_time(&local_time, "%Y-%m-%d_%H-%M-%S");
+    return ss.str();
+}
+
+bool run_suricata(const std::string& pcap_file, const std::string& base_output_dir)
+{
+    std::string output_dir = generate_output_dir(base_output_dir);
+
     try //create output folder if not exist
     {
         if (!std::filesystem::exists(output_dir))
